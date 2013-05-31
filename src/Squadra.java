2399a2b36c8e4b08793884564d65daf703013a01
simone <sic2@st-andrews.ac.uk>
2013-05-31 14:10:27 +0100

    ---

--- src/Squadra.java
+++ src/Squadra.java
@@ -13,62 +13,62 @@ abstract public class  Squadra{
 	int golfatti;
 	int golsubiti;
 	int punti;
-		
+
 	int totalepronostico; //serve per il pronostico, sara la somma dei gol fatti, subiti, vittorie sconfitte, pareggi + le abilita totali di difesa,attacco,centrocampo
 	int abilitaDifesa; //serve per il pronostico
 	int abilitaCentrocampo; //serve per il pronostico
 	int abilitaAttacco; //serve per il pronostico
 	//FINE MODIFICA
-	
-	
+
+
 	//Costruttore di Squadra!!!
 	public Squadra(ArrayList <Giocatore> vett, String nome){
-		    miasquadra = vett;//aggiunta con array dinamici
-			this.nome = nome;
-			totalepronostico = 0;
-			
-			
-			/*non faccio altro che mettere nelle variabili abilita,  tutte le abilita specifiche di ogni giocatore alla fine le variabili abilita
-			 * avranno la somma di tutte le abilita dei giocatori (difensori attaccanti centrocampisi)
-			 * ciò è utile per il pronostico*/
-			int d=0,c=0,a=0; 
-			double b=0; // calcolare il budget
-			for(Giocatore i : miasquadra){
-				if(i instanceof Difensore) {
-					abilitaDifesa += ((Difensore)i).getAbilitaDifesa();
+		miasquadra = vett;//aggiunta con array dinamici
+		this.nome = nome;
+		totalepronostico = 0;
+
+
+		/*non faccio altro che mettere nelle variabili abilita,  tutte le abilita specifiche di ogni giocatore alla fine le variabili abilita
+		 * avranno la somma di tutte le abilita dei giocatori (difensori attaccanti centrocampisi)
+		 * ciò è utile per il pronostico*/
+		int d=0,c=0,a=0; 
+		double b=0; // calcolare il budget
+		for(Giocatore i : miasquadra){
+			if(i instanceof Difensore) {
+				abilitaDifesa += ((Difensore)i).getAbilitaDifesa();
+				d++;
+				b+=((Difensore)i).getValoreMercato();
+			}
+			else{
+				if(i instanceof Portiere){
+					abilitaDifesa += ((Portiere)i).getValoreGenerale();
 					d++;
-					b+=((Difensore)i).getValoreMercato();
-				}
-				else{
-					if(i instanceof Portiere){
-						abilitaDifesa += ((Portiere)i).getValoreGenerale();
-						d++;
-						b+=((Portiere)i).getValoreMercato();
-					}
-				}
-				
-				if(i instanceof Centrocampista){
-					abilitaCentrocampo += ((Centrocampista)i).getAbilitaCentrocampo();
-					c++;
-					b+=((Centrocampista)i).getValoreMercato();
-				}
-				if(i instanceof Attaccante){
-					abilitaAttacco += ((Attaccante)i).getAbilitaAttacco();
-					a++;
-					b+=((Attaccante)i).getValoreMercato();
+					b+=((Portiere)i).getValoreMercato();
 				}
 			}
-			abilitaDifesa/=d;
-			abilitaCentrocampo/=c;
-			abilitaAttacco/=a;
-			this.budget = b + (double)(Math.random()*1000)+1;
-		
+
+			if(i instanceof Centrocampista){
+				abilitaCentrocampo += ((Centrocampista)i).getAbilitaCentrocampo();
+				c++;
+				b+=((Centrocampista)i).getValoreMercato();
+			}
+			if(i instanceof Attaccante){
+				abilitaAttacco += ((Attaccante)i).getAbilitaAttacco();
+				a++;
+				b+=((Attaccante)i).getValoreMercato();
+			}
+		}
+		abilitaDifesa/=d;
+		abilitaCentrocampo/=c;
+		abilitaAttacco/=a;
+		this.budget = b + (double)(Math.random()*1000)+1;
+
 	}
 	//serve per il pronostico, sara la somma dei gol fatti, subiti, vittorie sconfitte, pareggi + le abilita totali di difesa,attacco,centrocampo
 	public void setTotalePronostico(){
 		totalepronostico = (getVittorie()+ getPareggi()-getSconfitte()+getGolFatti()-getGolSubiti() + getAbilitaCentrocampo()+getAbilitaAttacco()+getAbilitaDifesa());
 	}
-	
+
 	private int getAbilitaDifesa() {
 		return abilitaDifesa;
 	}
@@ -78,9 +78,9 @@ abstract public class  Squadra{
 	private int getAbilitaCentrocampo() {
 		return abilitaCentrocampo;
 	}
-	
+
 	//FINE MODIFICA
-	
+
 	private int getGolSubiti() {
 		return golsubiti;
 	}
@@ -102,17 +102,23 @@ abstract public class  Squadra{
 	}
 
 	public void SetVittorie(){
-    	 vittorie++;
-    }
-	
+		vittorie++;
+	}
+
 	public void SetSconfitte(){
-    	sconfitte++;
-    }
-	
+		sconfitte++;
+	}
+
 	public void SetPareggi(int p){
-    	pareggi++;
-    }
-	
+		pareggi++;
+	}
+
+
+	// XXX - Aggiunto da Simon
+	public ArrayList <Giocatore> getGiocatori() {
+		// TODO
+		return null;
+	}
 	
 	//MODIFICTO  14 MAGGIO ORE 18:00
 
@@ -121,55 +127,55 @@ abstract public class  Squadra{
 	}
 	//FINE MODIFICA
 	public String GetNomeSquadra(){
-    	return nome;
-    }
-	
+		return nome;
+	}
+
 	public void SetGolfatti(){
-    	golfatti++;
-    }
+		golfatti++;
+	}
 	public void setGolSubiti(){
-    	golsubiti++;
-    }
-	
-	
-	
+		golsubiti++;
+	}
+
+
+
 	abstract public void scambio(String Cognomes, String Cognomes1, Squadra s1);
 	abstract public void acquisto(String Cognome,Squadra s1);
 	abstract public Giocatore[] OrganizzaSquadra();
 	//CREDO CHE CREARE IL METODO VENDITA SIA INUTILE... DATO CHE ABBIAMO ACQUISTI CHE IN UN CERTO SENSO GUARDANDOLA DAL 
 	//PUNTO DI VISTA DELLA SQUADRAS1 È UNA VENDITA..
-	
+
 	///////////////////////////////////////////////****************************************/////////////////////////////////////////////	
-	
-	
-	
+
+
+
 	protected Giocatore Search(Squadra s,String Cognome){
 		for(Giocatore i : s.GetSquadra()){
 			if(i.GetAnagrafe().GetCognome().equalsIgnoreCase(Cognome)){
 				return i;
-			 }
+			}
 		}
 		return null;
 	}
-	
+
 	///////////////////////////////////////////////////************************************/////////////////////////////////////
-	
-	
+
+
 	public ArrayList<Giocatore> GetSquadra(){
 		return miasquadra;
 	}
-	
+
 	public double GetBudget(){
 		return budget;
 	}
-	
+
 	public double SetBudgetAdd(double z){
 		return budget+z;
 	}
 	public double SetBudgetRemove(double z){
 		return budget-z;
 	}
-	
+
 	//MODIFICTO  14 MAGGIO ORE 18:00
 
 	//////////////////che ve ne pare!!?!?!!?
@@ -179,7 +185,7 @@ abstract public class  Squadra{
 				int rdm = (int)(Math.random()*10);
 				if(rdm <5) System.out.println("Il pronostico è a favore della squadra "+a.GetNomeSquadra());
 				else System.out.println("Il pronostico è a favore della squadra "+b.GetNomeSquadra());
-			 }
+			}
 			else {
 				int fortuna = (int)(Math.random()*2);
 				int rdm = (int)(Math.random()*10)+fortuna;
@@ -188,12 +194,12 @@ abstract public class  Squadra{
 			}
 		}
 		else if (b.getTotalePronostico() == 0){
-			
+
 			int fortuna = (int)(Math.random()*2);
 			int rdm = (int)(Math.random()*10)+fortuna;
 			if(rdm <=3) System.out.println("Il pronostico è a favore della squadra "+b.GetNomeSquadra());
 			else System.out.println("Il pronostico è a favore della squadra "+a.GetNomeSquadra());
-			}
+		}
 		else if(a.getTotalePronostico()<b.getTotalePronostico()){
 			int fortuna = (int)(Math.random()*2);
 			int rdm = (int)(Math.random()*10)+fortuna;
